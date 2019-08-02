76190319397045e8a24abc0ad52180cab0497969
electrocile <yuj022209@outlook.com>
2019-08-02 11:34:58 +0800

    Updated comments.

--- app/src/main/java/com/example/hob/HomeActivity.java
+++ app/src/main/java/com/example/hob/HomeActivity.java
@@ -41,14 +41,15 @@ import java.util.NavigableMap;
 
 public class HomeActivity extends AppCompatActivity{
 
+    // variables for slidingPanelLayout.
     SlidingPaneLayout mSlidier;
     View mMenuNavi;
     View mScrollView;
-
+    // variables for sprite animation.
     ImageView imageRun;
     AnimationDrawable spriteRun;
     AnimationDrawable spriteJump;
-
+    // introduce handler for delay.
     public Handler handler;
     public Runnable runnable;
 
@@ -58,22 +59,26 @@ public class HomeActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // set up default sprite animation.
         imageRun = findViewById(R.id.spriteRunning);
         imageRun.setImageResource(R.drawable.animation_1);
         spriteRun=(AnimationDrawable)imageRun.getDrawable();
         spriteRun.start();
 
-        mSlidier= findViewById(R.id.mainLayout);
-        mMenuNavi = findViewById(R.id.menuNavi);
-        mScrollView = findViewById(R.id.scrollView);
-
-
+        // make list for images displayed in banner ads.
         Integer[] imageAd ={R.drawable.balloon, R.drawable.beach, R.drawable.camp, R.drawable.mountain};
+        // set up banner.
         Banner banner = findViewById(R.id.banner);
         banner.setImageLoader(new GlideImageLoader());
         banner.setImages(Arrays.asList(imageAd));
         banner.start();
 
+        // define variables in slidingPanelLayout.
+        mSlidier= findViewById(R.id.mainLayout);
+        mMenuNavi = findViewById(R.id.menuNavi);
+        mScrollView = findViewById(R.id.scrollView);
+        // fade-in animation for navigation menu.
         mMenuNavi.setAlpha(0);
         mSlidier.setPanelSlideListener(new SlidingPaneLayout.PanelSlideListener() {
             @Override
@@ -92,6 +97,7 @@ public class HomeActivity extends AppCompatActivity{
             }
         });
 
+        // make lists for icons and labels displayed in navigation menu.
         int[] imageId = new int[]{R.drawable.icon_profile, R.drawable.icon_news, R.drawable.icon_mail, R.drawable.icon_promo, R.drawable.icon_contact,  R.drawable.icon_setting};
         String[] name = new String[] {"Profile", "News", "Mailbox", "Promos", "Contact us", "Preference"};
         List<Map<String, Object>> listItem = new ArrayList<>();
@@ -102,44 +108,61 @@ public class HomeActivity extends AppCompatActivity{
             map.put("name", name[i]);
             listItem.add(map);
             }
+        // set up ListView using adapter.
         SimpleAdapter adapter = new SimpleAdapter(this, listItem, R.layout.list_layout, new String[]{"name", "image"}, new int[]{R.id.title, R.id.image});
         ListView listView = super.findViewById(R.id.menuListView);
         listView.setAdapter(adapter);
     }
 
+    /**
+     * Onclick method to open navigation menu in slidingPanelLayout other than sliding gesture.
+     */
     public void slideOpen(View view){
+
         mSlidier.openPane();
     }
 
-
+    /**
+     * Onclick method to toggle hidden account details.
+     */
+    // tag for state control.
     public int tag1=0;
     public void moreAccount(View view){
+        // rotate animation for arrow "show more" and "show less"
         Animation rotateDown = AnimationUtils.loadAnimation(this, R.animator.rotate_down);
         Animation rotateUp = AnimationUtils.loadAnimation(this, R.animator.rotate_up);
         ConstraintLayout hiddenAccount=findViewById(R.id.hiddenAccount);
         if (tag1 == 0) {
+            // less account shown, rotate arrow and show hidden account.
             view.startAnimation(rotateDown);
             hiddenAccount.setVisibility(View.VISIBLE);
             tag1 = 1;
         } else {
+            // all accounts shown, rotate arrow and show less account.
             view.startAnimation(rotateUp);
             hiddenAccount.setVisibility(View.GONE);
             tag1 = 0;
         }
     }
 
+    /**
+     * Onclick method to toggle sprite animation.
+     */
     public void clickJump(View view){
+        // translate ImageView up and down to simulate jumping displacement.
         Animation jumpTranslate = AnimationUtils.loadAnimation(this, R.animator.jump);
         view.startAnimation(jumpTranslate);
+        // change sprite animation to jump.
         imageRun = findViewById(R.id.spriteRunning);
         imageRun.setImageResource(R.drawable.animation_2);
         spriteJump=(AnimationDrawable)imageRun.getDrawable();
         spriteJump.start();
-
+        // create a handler for delay.
         handler = new Handler();
         handler.postDelayed(runnable = new Runnable() {
             @Override
             public void run() {
+                // wait until jump finish, set back to running animation again.
                 imageRun= findViewById(R.id.spriteRunning);
                 imageRun.setImageResource(R.drawable.animation_1);
                 spriteRun=(AnimationDrawable)imageRun.getDrawable();
